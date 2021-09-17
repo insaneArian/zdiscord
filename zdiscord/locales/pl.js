62b8c7b39cc0dbe54a5f08d3d71882f22104152a
insaneArian <90046684+insaneArian@users.noreply.github.com>
2021-09-17 17:33:34 +0200

    Update pl.js

--- zdiscord/locales/pl.js
+++ zdiscord/locales/pl.js
@@ -7,7 +7,7 @@ const locale = {
     checkingWhitelist: "Siema {{name}}, Sprawdzamy czy jesteś na whiteliście..", // Globals + {{name}}
     discordNotOpen: "Musisz mieć włączonego Discorda przed FiveM'em (zrestartuj oba jeśli problem dalej występuje)", // Globals
     fatalError: "Coś poszło nie tak przy sprawdzaniu twojej whitelisty, spróbuj jeszcze raz za chwilę, a jeżeli to nie pomoże skontaktuj się z administracją.", // Globals
-    notInDiscordServer: "Nie jesteś na discordzie {{servername}}, dołącz tutaj: {{invite}}", // Globals
+    notInDiscordServer: "Nie jesteś jeszcze na discordzie {{servername}}, dołącz tutaj: {{invite}}", // Globals
     notWhitelisted: "Nie masz odpowiedniej roli na discordzie {{servername}} aby dołączyć na serwer, jesteś pewny, że masz whiteliste?", // Globals
     kickedWithoutReason: "Zostałeś wyrzucony przez administrację.", // Globals
     announcement: "OGŁOSZENIE",
